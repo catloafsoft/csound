80dc6001363c09ada54397312d973f196a40fb05
Stephane Peter <megastep@megastep.org>
2015-04-13 18:19:44 -0700

    Updated AudioKit script to abort if compilation fails, more versatile name change on CsoundLib

--- installer/audiokit/release-build-osx.sh
+++ installer/audiokit/release-build-osx.sh
@@ -5,6 +5,14 @@
 # (c) 2015 Stephane Peter
 #
 
+#
+# Note: libsndfile needs to be installed on the system for this compilation to succeed.
+# Easy way using Fink: fink install libsndfile1-dev
+# Then copy the universal libraries already contained from the AudioKit CsoundFile.framework/lib/lib*.dylib to /sw/lib/
+# This should allow the compilation to finish successfully.
+#
+
+
 # Default: AudioKit in the same parent directory as csound
 AK_ROOT=${AK_ROOT:-$PWD/../../../AudioKit/AudioKit/Platforms/OSX}
 BUILD_TYPE=${BUILD_TYPE:-Release}
@@ -20,14 +28,14 @@ cp Custom-ak.cmake ../../Custom.cmake
 mkdir osx
 cd osx
 
-cmake ../../.. -DBUILD_INSTALLER=1 -DCMAKE_INSTALL_PREFIX=$BUILD_TYPE -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_OSX_ARCHITECTURES="i386;x86_64" -DBUILD_TESTS=0 -DBUILD_CSOUND_AC=1 -DBUILD_FAUST_OPCODES=1 -DUSE_DOUBLE=0 -DUSE_CURL=0 -DBUILD_IMAGE_OPCODES=0
-make -j6 install
+cmake ../../.. -DBUILD_INSTALLER=1 -DCMAKE_INSTALL_PREFIX=$BUILD_TYPE -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_OSX_ARCHITECTURES="i386;x86_64" -DBUILD_TESTS=0 -DBUILD_CSOUND_AC=1 -DBUILD_FAUST_OPCODES=1 -DUSE_DOUBLE=0 -DUSE_CURL=0 -DBUILD_IMAGE_OPCODES=0 || exit 1
+make -j6 install || exit 1
 
 # Copy new libraries and headers for Csound and its opcodes to the AudioKit framework
 cd $BUILD_TYPE/CsoundLib.framework
 
 # Change the library name for libsndfile in CSoundLib and libcsound.dylib
-install_name_tool -change /Users/megastep/src/libsndfile/src/Debug/libsndfile.1.dylib @loader_path/../../libs/libsndfile.1.dylib CsoundLib
+install_name_tool -change `otool -LX CsoundLib |grep libsndfile.1|awk '{print $1}'` @loader_path/../../libs/libsndfile.1.dylib CsoundLib
 cp CsoundLib ${AK_ROOT}/CsoundLib.framework/
 
 cp Headers/* ${AK_ROOT}/../Common/csound/
